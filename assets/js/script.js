9d757054138173173b12dedaf70f0a33b4429d6a
ajcudd <ajcuddeback@gmail.com>
2020-11-18 16:56:22 -0500

    added local stoarge

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,74 @@ var formEl = document.querySelector(".search-form");
 var citySearchEl = document.querySelector("#city");
 var currentDayContainer = document.querySelector(".current-day-content")
 var fiveDayWrapper = document.querySelector(".five-day-wrapper");
+var historyWrapper = document.querySelector(".history")
+
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var cityName = citySearchEl.value.trim();
+    cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+    
     if (cityName) {
         getCurrentCityWeather(cityName);
         getFiveDayForcast(cityName);
         citySearchEl.value = "";
+        createHistoryButton()
     } else {
         alert("Please input a city!");
+        formSubmitHandler()
     }
-
 };
 
+var saveCities = function (cityObj) {
+    var currentCity = JSON.parse(localStorage.getItem('cityItems')) || [];
+    currentCity.push(cityObj);
+    localStorage.setItem('cityItems', JSON.stringify(currentCity))
+}
+
+
+
+var historyClickHandler = function (event) {
+    var historyEl = event.target.getAttribute("data-city");
+    if (historyEl) {
+        getCurrentCityWeather(historyEl);
+        getFiveDayForcast(historyEl)
+    }
+}
+
 var getCurrentCityWeather = function (city) {
+    var cityObj = {
+        city: city
+    }
+
     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0938c05e8d987103f9ba5cb07b6b876e&units=imperial";
     fetch(weatherApiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayCurrentCityWeather(data);
                 getCurrentUvIndex(data);
+                saveCities(cityObj)
             })
         } else {
             alert("Please insert a valid city!");
         }
     });
+    createHistoryButton()
 };
+
+var createHistoryButton = function() {
+    var cityName = JSON.parse(localStorage.getItem('cityItems')) || [];
+    
+    historyWrapper.innerHTML = "";
+    cityName.forEach(element => {
+        var historyBtn = document.createElement("button");
+        historyBtn.className = "historybtn"
+        historyBtn.textContent = element.city;
+        historyBtn.setAttribute("data-city", element.city)
+        historyWrapper.appendChild(historyBtn);
+    })
+}
+createHistoryButton()
 
 var getFiveDayForcast = function (city) {
     var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=0938c05e8d987103f9ba5cb07b6b876e&units=imperial";
@@ -36,7 +77,6 @@ var getFiveDayForcast = function (city) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayFiveDayForcast(data);
-                console.log(data)
             });
         } else {
             alert("Could not find 5-day forcast")
@@ -50,8 +90,7 @@ var getCurrentUvIndex = function (data) {
     fetch(uvApiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                displayCurrentUvIndex(data);
-                console.log(data);
+                displayCurrentUvIndex(data);;
             });
         } else {
             alert('There was a problem fetching UV Index!');
@@ -96,9 +135,9 @@ var displayFiveDayForcast = function (data) {
         fiveDayTemp.textContent = "Temp: " + Math.floor(data.list[i].main.temp) + " F";
         fiveDayCard.appendChild(fiveDayTemp);
 
-        var fiveDayHumidity =  document.createElement("p");
+        var fiveDayHumidity = document.createElement("p");
         fiveDayHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
-        fiveDayCard.appendChild(fiveDayHumidity)
+        fiveDayCard.appendChild(fiveDayHumidity);
     }
 }
 
@@ -121,3 +160,4 @@ var displayCurrentUvIndex = function (data) {
 }
 
 formEl.addEventListener("submit", formSubmitHandler);
+historyWrapper.addEventListener("click", historyClickHandler);
