dcf04f9a5b7f5403382c0ca0043f2666038efe84
ajcudd <ajcuddeback@gmail.com>
2020-11-17 22:44:48 -0500

    added ability to view temp, wind, and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
-var formEl = document.querySelector(".search-form")
+var formEl = document.querySelector(".search-form");
+var citySearchEl = document.querySelector("#city");
+var currentDayContainer = document.querySelector(".current-day-content")
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    var cityName = document.querySelector("#city").value.trim();
+    var cityName = citySearchEl.value.trim();
     if (cityName) {
         getCityWeather(cityName)
+        citySearchEl.value = "";
     } else {
         alert("Please input a city!")
     }
-}
+    
+};
 
 var getCityWeather = function(city) {
-    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=0938c05e8d987103f9ba5cb07b6b876e";
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=0938c05e8d987103f9ba5cb07b6b876e&units=imperial";
     fetch(weatherApiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
+            displayCityWeather(data);
             console.log(data)
             })
         } else {
             alert("Please insert a valid city!")
         }
     });
+};
+
+var displayCityWeather = function(data) {
+    var currentCityEl = document.querySelector("#current-city");
+    var tempEl = document.querySelector("#temp");
+    var humidityEl = document.querySelector("#humidity");
+    var windEl = document.querySelector("#wind");
+    var uvEl = document.querySelector("#uv");
+
+    var currentCity = data.name;
     
+    currentCityEl.textContent = currentCity + "    " + dayjs().format('MM/DD/YYYY');
+    tempEl.textContent = Math.floor(data.main.temp);
+    humidityEl.textContent = data.main.humidity;
+    windEl.textContent = data.wind.speed;
 }
 
-formEl.addEventListener("submit", formSubmitHandler)
+formEl.addEventListener("submit", formSubmitHandler);
